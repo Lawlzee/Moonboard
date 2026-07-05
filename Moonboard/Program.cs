6faca87f3c646f928e22248d161b44ef1ed0c724
Olivier Lavoie <olivier.lavoie@nosotech.com>
2026-07-05 14:15:51 -0400

    refactoring

--- Moonboard/Program.cs
+++ Moonboard/Program.cs
@@ -1,26 +1,5 @@
 ﻿using Moonboard;
 
 string windowTitle = "OnePlus Nord N30 5G";
-string outputPath = "../../../Output";
-
 Phone phone = new Phone(windowTitle);
-phone.Focus();
-
-for (int i = 0; i < 1000; i++)
-{
-    if (Keyboard.IsF12Down())
-    {
-        return;
-    }
-
-    /*int duplicateHashCount = phone.TakeScreenshot(Path.Combine(outputPath, i + ".png"));
-
-    if (duplicateHashCount > 9)
-    {
-        return;
-    }*/
-
-    phone.TakeScreenshot(Path.Combine(outputPath, i + ".png"));
-    phone.SwipeRight();
-    Thread.Sleep(200);
-}
+new Scrapper(phone).Scrap("V3", 818);
---
+++ Moonboard/Scrapper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moonboard;
+
+public class Scrapper
+{
+    private readonly Phone _phone;
+
+    public Scrapper(Phone phone)
+    {
+        _phone = phone;
+    }
+
+    public void Scrap(string grade, int problemCount)
+    {
+        string outputPath = Path.Combine( "../../../Output", grade);
+
+        _phone.Focus();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            if (Keyboard.IsF12Down())
+            {
+                return;
+            }
+
+            /*int duplicateHashCount = phone.TakeScreenshot(Path.Combine(outputPath, i + ".png"));
+
+            if (duplicateHashCount > 9)
+            {
+                return;
+            }*/
+
+            _phone.TakeScreenshot(Path.Combine(outputPath, i + ".png"));
+            ph_phoneone.SwipeRight();
+            Thread.Sleep(200);
+        }
+    }
+}
